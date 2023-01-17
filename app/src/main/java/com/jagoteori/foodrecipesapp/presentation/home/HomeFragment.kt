16a7eca3b9b60f9990099dc8869740801e92216a
Muhammad Ridhoi <r.ridoi702@gmail.com>
2023-01-17 13:44:27 +0800

    Compose Migration: item recommendation home page

--- app/src/main/java/com/jagoteori/foodrecipesapp/presentation/home/HomeFragment.kt
+++ app/src/main/java/com/jagoteori/foodrecipesapp/presentation/home/HomeFragment.kt
@@ -7,9 +7,30 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.bumptech.glide.Glide
+import coil.compose.AsyncImage
 import com.google.android.material.tabs.TabLayoutMediator
 import com.jagoteori.foodrecipesapp.R
 import com.jagoteori.foodrecipesapp.data.Resource
@@ -18,6 +39,8 @@ import com.jagoteori.foodrecipesapp.databinding.ItemRecommendationRecipeBinding
 import com.jagoteori.foodrecipesapp.domain.entity.RecipeEntity
 import com.jagoteori.foodrecipesapp.presentation.adapter.GenericListAdapter
 import com.jagoteori.foodrecipesapp.presentation.detail_recipe.DetailRecipeActivity
+import com.jagoteori.foodrecipesapp.presentation.ui.theme.BlackColor500
+import com.jagoteori.foodrecipesapp.presentation.ui.theme.GreyColor300
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import timber.log.Timber
 
@@ -59,7 +82,7 @@ class HomeFragment : Fragment() {
         for (index in 0 until tabs.childCount) {
             Timber.d("ikang :: $index")
             val tab = tabs.getChildAt(index)
-            val layoutParams =  tab.layoutParams as LinearLayout.LayoutParams
+            val layoutParams = tab.layoutParams as LinearLayout.LayoutParams
             layoutParams.marginEnd = 30
         }
         binding.tabs.requestLayout()
@@ -71,22 +94,95 @@ class HomeFragment : Fragment() {
             R.layout.item_recommendation_recipe,
             bind = { item, holder, _ ->
                 val binding = ItemRecommendationRecipeBinding.bind(holder.itemView)
-                with(binding) {
-                    tvTitleRecipe.text = item.title
-                    tvPublisher.text = item.publisher
-                    Glide.with(requireActivity()).asBitmap().load(item.recipePicture)
-                        .into(imgRecipe)
-                }
-
-                holder.itemView.setOnClickListener {
-                    val intent = Intent(context, DetailRecipeActivity::class.java).putExtra(
-                        DetailRecipeActivity.DATA_RECIPE,
-                        item
-                    )
-                    startActivity(intent)
+                binding.composeView.setContent {
+                    MaterialTheme {
+                        ItemRecommendation(
+                            title = item.title,
+                            publisher = item.publisher,
+                            imageRecipe = item.recipePicture
+                        ) {
+                            val intent = Intent(context, DetailRecipeActivity::class.java).putExtra(
+                                DetailRecipeActivity.DATA_RECIPE,
+                                item
+                            )
+                            startActivity(intent)
+                        }
+                    }
                 }
             }
         ) {}
+    }
+
+    @Composable
+    fun ItemRecommendation(
+        title: String?,
+        publisher: String?,
+        imageRecipe: String?,
+        itemOnClick: () -> Unit
+    ) {
+        ConstraintLayout(modifier = Modifier.clickable { itemOnClick() }) {
+            val (idImageRecipe, idTitle, idPublisher) = createRefs()
+
+            Box(modifier = Modifier
+                .clip(RoundedCornerShape(12))
+                .constrainAs(idImageRecipe) {
+                    top.linkTo(parent.top)
+                    start.linkTo(parent.start)
+                }) {
+                AsyncImage(
+                    model = imageRecipe,
+                    contentDescription = null,
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .size(220.dp, 120.dp)
+                )
+                Surface(
+                    shape = RoundedCornerShape(bottomStart = 10.dp, topEnd = 22.dp),
+                    color = BlackColor500,
+                    modifier = Modifier
+                        .width(220.dp)
+                        .height(120.dp)
+                        .alpha(0.15f)
+                ) {}
+            }
+
+            Text(
+                text = title ?: "",
+                overflow = TextOverflow.Ellipsis,
+                textAlign = TextAlign.Start,
+                maxLines = 2,
+                fontSize = 16.sp,
+                color = BlackColor500,
+                modifier = Modifier.constrainAs(idTitle) {
+                    top.linkTo(idImageRecipe.bottom, margin = 8.dp)
+                    start.linkTo(parent.start, margin = 8.dp)
+                    end.linkTo(parent.end, margin = 8.dp)
+                    width = Dimension.fillToConstraints
+                }
+            )
+
+            Text(
+                text = publisher ?: "",
+                overflow = TextOverflow.Ellipsis,
+                textAlign = TextAlign.Start,
+                maxLines = 2,
+                fontSize = 14.sp,
+                color = GreyColor300,
+                modifier = Modifier.constrainAs(idPublisher) {
+                    top.linkTo(idTitle.bottom, margin = 4.dp)
+                    start.linkTo(idTitle.start)
+                    bottom.linkTo(parent.bottom, margin = 8.dp)
+                }
+            )
+        }
+    }
+
+    @Preview(showBackground = true)
+    @Composable
+    fun NewsItemPreview() {
+        MaterialTheme {
+            ItemRecommendation("asdas", "New News", "2022-02-22") {}
+        }
     }
 
     private fun showRecyclerList() {
---
+++ app/src/main/java/com/jagoteori/foodrecipesapp/presentation/ui/theme/Colors.kt
@@ -0,0 +1,12 @@
+package com.jagoteori.foodrecipesapp.presentation.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+
+val GreyColor300  = Color(0xFFADA8A4)
+val GreyColor100  = Color(0xFFDDDBDA)
+val GreyColor500  = Color(0xFF7B756F)
+val BlackColor500  = Color(0xFF191410)
+val BlackColorBody  = Color(0xFF2F323A)
+val WhiteColor  = Color(0xFFFFFFFF)
+val BackgroundColor  = Color(0xFFFAFAFA)
